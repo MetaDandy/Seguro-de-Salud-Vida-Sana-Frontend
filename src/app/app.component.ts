81f4b48d3fc863b25bde201e4c256be5e149dc27
Joseph Benitez Arroyo <benitezarroyojoseph@gmail.com>
2024-11-01 09:43:22 -0400

    probando angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './Test/test/test.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Seguro-de-Salud-Vida-Sana';
+  condition = true;
+  ar = [1, 2, 3, 4];
 }
