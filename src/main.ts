4af37376a5e5c8f8f31923cfc8384968f0dac347
Joseph Benitez Arroyo <benitezarroyojoseph@gmail.com>
2024-11-09 16:28:06 -0400

    a

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserAnimationsModule, // Necesario para las animaciones de ngx-toastr
+      ToastrModule.forRoot({
+        timeOut: 3000, // Duración de la notificación
+        positionClass: 'toast-top-right', // Ubicación en la pantalla
+        preventDuplicates: true, // Evita mostrar duplicados
+      })
+    ),
+    ...appConfig.providers,
+  ],
+}).catch((err) => console.error(err));
