2b12361cbeab57c792c6538573382b15fd946fad
MetaDandy <benitezarroyojoseph@gmail.com>
2024-11-14 17:36:18 -0400

    some fixes

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiurl: 'https://ec2-18-118-8-106.us-east-2.compute.amazonaws.com/',
+  apiurl: 'https://ec2-18-118-8-106.us-east-2.compute.amazonaws.com',
 };
