ec87f74f2f8e9712ddc1c49cd0e0e648d9e16d60
MetaDandy <benitezarroyojoseph@gmail.com>
2024-11-17 00:53:11 -0400

    pwa

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -15,6 +16,9 @@ bootstrapApplication(AppComponent, {
         preventDuplicates: true, // Evita mostrar duplicados
       })
     ),
-    ...appConfig.providers,
+    ...appConfig.providers, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 }).catch((err) => console.error(err));
