9b4d9caea364698332dd1ed5c2a61a60c5a114fa
MetaDandy <benitezarroyojoseph@gmail.com>
2024-11-17 13:39:02 -0400

    proxys

---
+++ functions/proxy.js
@@ -0,0 +1,50 @@
+// const fetch = require("node-fetch"); // Si usas node-fetch
+const fetch = globalThis.fetch; // Si usas Node.js v18+
+
+exports.handler = async (event) => {
+  console.log("Request received:", JSON.stringify(event, null, 2));
+
+  const targetUrl = `http://ec2-52-15-169-133.us-east-2.compute.amazonaws.com/api${event.path.replace(
+    "/api",
+    ""
+  )}`;
+
+  console.log("Target URL:", targetUrl);
+
+  try {
+    // Agregamos un timeout de 10 segundos (10000 ms)
+    console.log("Making request to target URL...");
+    const response = await fetch(targetUrl, {
+      method: event.httpMethod,
+      headers: {
+        "Content-Type": "application/json",
+        ...event.headers,
+      },
+      body: event.httpMethod === "GET" ? undefined : event.body,
+      timeout: 10000, // 10 segundos de timeout
+    });
+
+    console.log("Response received from target URL.");
+    const responseBody = await response.text();
+
+    console.log("Response status:", response.status);
+    console.log("Response body:", responseBody);
+
+    return {
+      statusCode: response.status,
+      body: responseBody,
+    };
+  } catch (error) {
+    console.error("Error in proxy:", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Error in proxy function",
+        details: error.message,
+      }),
+    };
+  } finally {
+    console.log("Proxy function execution completed.");
+  }
+};
--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiurl: 'http://ec2-18-118-8-106.us-east-2.compute.amazonaws.com',
+  apiurl: '/api',
 };
 //http://localhost:2424
 //http://ec2-18-118-8-106.us-east-2.compute.amazonaws.com
