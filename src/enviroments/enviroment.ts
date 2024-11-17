4df96dbf22adb53bd928c314cd0506c76b317eae
MetaDandy <benitezarroyojoseph@gmail.com>
2024-11-17 00:58:18 -0400

    url del backend

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiurl: 'http://localhost:2424',
+  apiurl: 'https://system-medical.onrender.com',
 };
 //http://localhost:2424
 //http://ec2-18-118-8-106.us-east-2.compute.amazonaws.com
+//https://system-medical.onrender.com
