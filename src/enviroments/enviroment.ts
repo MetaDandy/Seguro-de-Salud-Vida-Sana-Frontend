3120ae0cc7b8f9e75408ac4e2debdd9f93e34c48
MetaDandy <benitezarroyojoseph@gmail.com>
2024-11-17 11:46:54 -0400

    aws

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiurl: 'https://system-medical.onrender.com',
+  apiurl: 'http://ec2-18-118-8-106.us-east-2.compute.amazonaws.com',
 };
 //http://localhost:2424
 //http://ec2-18-118-8-106.us-east-2.compute.amazonaws.com
